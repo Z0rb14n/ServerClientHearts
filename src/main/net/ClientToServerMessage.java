09b542182727544424148967ccf0ccb93ed49c09
Z0rb14n <babybluewoof@gmail.com>
2020-05-04 23:01:40 -0700

    Setup Message formats

--- src/main/net/ClientToServerMessage.java
+++ src/main/net/ClientToServerMessage.java
@@ -1,11 +1,18 @@
 package net;
 
+import util.Card;
 import util.Deck;
 
 import java.io.Serializable;
 
 public class ClientToServerMessage implements Serializable {
     private static final long serialVersionUID = 1L;
-    Deck cards;
-    String interestingMessage;
+
+    // in order of which it'll likely be used
+    boolean isChatMessage = false;
+    String chatMessage = ""; // send new messages
+
+    Deck cards = new Deck(); // send first three cards
+
+    Card card; // future cards played
 }
--- src/main/net/ServerToClientMessage.java
+++ src/main/net/ServerToClientMessage.java
@@ -1,11 +1,62 @@
 package net;
 
+import util.Card;
 import util.Deck;
+import util.Suit;
 
 import java.io.Serializable;
 
+// Represents the message sent from server to client
 public class ServerToClientMessage implements Serializable {
-    Deck center;
-    Deck urHand;
-    String interestingMessage;
+    private static final long serialVersionUID = 1L;
+    // in order of which it'll likely be used
+
+    // You have been kicked from connecting
+    boolean isKickMessage = false;
+    String kickMessage = "";
+
+    // ID string + player number
+    String ID = "";
+    int playerNumber = 0;
+
+    // Chat message
+    String chatMessage = "";
+    int playerChatSender = 0;
+
+    // New player connection
+    boolean playerConnect = false;
+    int newPlayerNumber = 0;
+
+    // Player disconnected
+    boolean playerDisconnect = false;
+    int disconnectedPlayerNumber = 0;
+
+    // Starting the game -> pass 3 cards and here's ur hand
+    boolean gameStarting = false;
+    Deck clientHand = new Deck();
+
+    // Start first turn (get 3 cards, and who starts first)
+    Deck threeNewCards = new Deck();
+    boolean startingFirstTurn = false;
+    boolean[] whichClientStarts = new boolean[4]; // determines who starts as a boolean [p1,p2,p3,p4]
+
+    // New card played, who played it, the suit to play, and next player number
+    Card previouslyPlayed;
+    int playerNumJustPlayed = 0;
+    Suit expectedSuit;
+    int nextPlayerNumber = 0;
+
+    // Starting new turn (who got rekt by penalties, what penalties)
+    boolean startingNewTurn = false;
+    Deck newPenaltyCards = new Deck();
+    int playerWhoGetsPenalties = 0;
+
+    // End of game (who won, what penalty points they had, what cards they had)
+    boolean gameEnding = false;
+    boolean[] winners = new boolean[4];
+    int[] penaltyPoints = new int[4];
+    Deck[] penaltyHands = new Deck[4];
+
+    // Reset
+    boolean isReset = false;
 }
