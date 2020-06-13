ff7d6b66817efd59b0c25ef4f10e498cee43c864
Z0rb14n <babybluewoof@gmail.com>
2020-06-13 00:33:54 -0700

    Removed unneeded initializer for ConnectionException

--- src/main/net/ConnectionException.java
+++ src/main/net/ConnectionException.java
@@ -2,8 +2,4 @@ package net;
 
 // Represents an exception thrown when you cannot connect.
 public class ConnectionException extends RuntimeException {
-    // EFFECTS: initializes exception with given message
-    ConnectionException(String msg) {
-        super(msg);
-    }
 }
